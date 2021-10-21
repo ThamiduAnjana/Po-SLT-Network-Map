787cb7cf5e1f9db7f6ab47e3efe20db954a80bc7
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-10-21 11:18:20 +0530

    Modify table

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,16 @@ function App() {
             <th>First Name</th>
             <th>Last Name</th>
             <th>Username</th>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Last Name</th>
+            <th>Username</th>
           </tr>
         </thead>
         <tbody>
@@ -23,17 +33,16 @@ function App() {
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Otto</td>
+            <td>@mdo</td>
           </tr>
         </tbody>
       </Table>
